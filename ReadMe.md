6371dc746d350dc90174c9b32a6d9f287c7016d6
Albert Michael Byrd <alxp2083@hotmail.com>
2017-04-21 16:22:10 -0400

    New Things

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
 # albertmpro 
 ------
 * Albert M. Byrd's Personal Website 
+* Also a place to learn new things. 
 
 
---
+++ dev/albert.js
@@ -0,0 +1,7 @@
+
+
+//Get the bassic id from a Dom Object
+function a(id)
+{
+    return document.getElementById(id);
+}
